9176cf1ce2ecec5129ee5b260b8cf763ef3b28ad
arya <aryatheruparambil@gmail.com>
2024-02-15 19:22:26 +0530

    file added

---
+++ camera/cameraApp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Product,Category
+from django.core.paginator import Paginator,EmptyPage,InvalidPage
+# Create your views here.
+       
+
+      
+def Home(request,c_slug=None):
+    c_page=None
+    product_list=None
+    if c_slug!=None:
+        c_page=get_object_or_404(Category,slug=c_slug)
+        print(c_page)
+        product_list=Product.objects.all().filter(category=c_page,available=True)
+    else:
+        product_list=Product.objects.all().filter(available=True) 
+  
+    paginator=Paginator(product_list,10)    
+    
+    try:
+        page=int(request.GET.get('page','1'))
+      
+    except:
+        page=1
+    try:
+        products=paginator.page(page) 
+    except( EmptyPage,InvalidPage):
+        products=paginator.page(paginator.num_pages) 
+        
+    return render(request,'index.html',{'data':product_list,'products':products,'category':c_page})        
+           
+
+
+
+
+def Details(req,id):
+    data=Product.objects.get(id=id)
+    return render(req,'details.html',{'data':data})
+
